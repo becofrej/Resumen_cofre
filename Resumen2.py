76cbac17d00b95e699eb62319b121cf5bdffac20
becofrej <136353373+becofrej@users.noreply.github.com>
2023-06-29 19:11:26 -0400

    Update Resumen2.py

--- Resumen2.py
+++ Resumen2.py
@@ -20,7 +20,7 @@ usuario["Nombre"] = nombre_usuario
 fecha_actual = input("Ingrese la fecha actual: ")
 usuario["Fecha"] = fecha_actual
 
-color_favorito = input("Ingrese su color fav: ")
+color_favorito = input("Ingrese su color fav: ").lower()
 usuario["Color"] = color_favorito
 
 
@@ -33,12 +33,71 @@ def buscar_modelo(lista):
     print(f"No se encontró el auto con modelo {modelo} en la lista.")
 
 
+def imprimir_lista(lista):
+    llave = input("Ingrese la llave: ")
+    parametro = input("Ingrese el valor: ")
+    print("Auto con esas caracteristicas:")
+    for auto in lista:
+        if llave in auto and auto[llave].lower() == parametro.lower():
+            print(auto)
+
+
 def imprimir_certificado(lista):
+    i = 0
+    for auto in lista:
+        i += 1
+        print(i, auto)
+    seleccion = int(input("Seleccione un vehiculo >>> "))
+    sel = seleccion - 1
+    auto_seleccionado = lista[sel]
+    if sel <= len(lista):
+        print(auto_seleccionado)
+        print(usuario['Nombre'], "emite certificado que:\nEl vehiculo", auto_seleccionado["marca"], auto_seleccionado["modelo"],
+              "con patente", auto_seleccionado["patente"] + "\n" "De color", auto_seleccionado["color"], "\nQueda registrado oficialmente a la fecha de", usuario["Fecha"])
+
+
+def buscar_patente(lista):
     patente = input("Ingrese su patente: ").lower()
     for auto in lista:
         if "patente" in auto and auto["patente"].lower() == patente:
-            print(usuario['Nombre'], "emite certificado que:\nEl vehiculo", auto["marca"], auto["modelo"],
-                  "con patente ", auto["patente"], "De color", auto["color"], "Queda registrado oficialmente a la fecha de", usuario["Fecha"])
+            auto_selec = auto
+            print(f"Su auto es: {auto}")
+            return auto_selec
+
+
+def buscar_año(lista):
+    año = input("¿Cual año busca?: ")
+    ultimos_2_digitos = año[-2:]
+    for auto in lista:
+        if "año" in auto and str(auto["año"])[-2:] == ultimos_2_digitos:
+            print(auto)
+
+
+def agregar_info_dueño(lista):
+    auto_seleccionado = buscar_patente(lista)
+    auto_seleccionado["nombre_propietario"] = input(
+        "Ingrese el nombre del usuario: ")
+    print("--- Ingreso realizado con exito ---")
+    return auto_seleccionado
+
+
+def buscar_por_color(lista):
+    encontrado = False
+    for auto in lista:
+        if "color" in auto and usuario["Color"] == auto["color"].lower():
+            if not encontrado:
+                print("Autos con su color favorito: ")
+                encontrado = True
+            print(auto)
+
+
+def buscar_por_nombre(lista):
+    propietario = input("Ingrese el propietario: ").lower()
+    for auto in lista:
+        if "nombre_propietario" in auto and auto["nombre_propietario"].lower() == propietario:
+            print(f"Auto asociado al propietario {propietario} : {auto}")
+        elif "nombre_propietario" in auto and auto["nombre_propietario"].lower() == usuario["Nombre"].lower():
+            print(f"Auto asociado al propietario {usuario['Nombre']} : {auto}")
 
 
 while True:
@@ -51,8 +110,25 @@ while True:
         buscar_modelo(lista_autos)
         break
     if opcion == 2:
-        imprimir_certificado(lista_autos)
+        imprimir_lista(lista_autos)
         break
     if opcion == 3:
-        print("Adios")
+        imprimir_certificado(lista_autos)
         break
+    if opcion == 4:
+        buscar_patente(lista_autos)
+        break
+    if opcion == 5:
+        buscar_año(lista_autos)
+        break
+    if opcion == 6:
+        auto_con_dueño = agregar_info_dueño(lista_autos)
+        break
+    if opcion == 7:
+        buscar_por_color(lista_autos)
+        break
+    if opcion == 8:
+        break
+
+# agregar_info_dueño(lista_autos)
+buscar_por_nombre(lista_autos)
